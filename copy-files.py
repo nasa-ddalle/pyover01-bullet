7d5a0ac480c5ab19abbf9381b54b66a0b064abe9
Derek J. Dalle <derek.j.dalle@nasa.gov>
2020-03-11 08:26:48 -0700

    Add remaining geom/grid files

---
+++ copy-files.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Standard library modules
+import os
+import shutil
+
+
+# This folder
+fdir = os.path.dirname(os.path.realpath(__file__))
+# Name of working folder
+fwork = os.path.join(fdir, "work")
+
+# Files list
+fnames = [
+    "pyOver.json",
+    "pyOver-completed.json",
+    "inputs/matrix.csv",
+    "inputs/buller-mach.lay",
+    "common/overflow.inp",
+    "common/fomo/mixsur.i",
+    "dcf/GlobalDefs.tcl",
+    "dcf/inputs.tcl",
+    "dcf/config.tcl",
+    "dcf/geom/bullet.lr8.crv",
+    "dcf/geom/bullet.i.tri",
+    "dcf/geom/bullet.stp",
+    "dcf/bullet/BuildBullet.tcl",
+    "dcf/bullet/localinputs.tcl",
+    "dcf/bullet/Makefile"
+]
+
+# Check if working folder exists
+if not os.path.isdir(fwork):
+    # Create it
+    os.mkdir(fwork)
+
+# Loop through files
+for fname in fnames:
+    # System independence
+    f1 = fname.replace("/", os.sep)
+    # Source file
+    fsrc = os.path.join(fdir, f1)
+    # Destination file
+    fdest = os.path.join(fwork, f1)
+    # Check if output file already exists
+    if os.path.isfile(fdest):
+        # Skip
+        continue
+    # Break out folder names (if any)
+    fparts = fname.split("/")[:-1]
+    # Create cumulative subfolder name
+    fsub = fwork
+    # Loop through subfolder names
+    for fpart in fparts:
+        # Add to subfolder name
+        fsub = os.path.join(fsub, fpart)
+        # Check if folder exists
+        if os.path.isdir(fsub):
+            continue
+        # Create folder
+        os.mkdir(fsub)
+    # Copy file
+    shutil.copy(fsrc, fdest)
+
